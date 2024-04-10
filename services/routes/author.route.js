3de05fcfa3108421cec0c7477618716d30bb828b
DeadCleaver <andrealuca.montefiori@gmail.com>
2024-04-10 19:28:20 +0200

    cancellate robe

--- services/routes/author.route.js
+++ services/routes/author.route.js
@@ -3,6 +3,7 @@ import Author from "../models/author.model.js";
 
 export const authorRoute = Router();
 
+/* chiamata get di tutti gli autori */
 authorRoute.get("/", async (req, res, next) => {
     try {
       const authors = await Author.find();
@@ -13,7 +14,7 @@ authorRoute.get("/", async (req, res, next) => {
     }
   });
 
-
+/* chiamata post di un autore */
   authorRoute.post("/", async (req, res, next) => {
     try {
       let author = await Author.create(req.body);
@@ -23,6 +24,7 @@ authorRoute.get("/", async (req, res, next) => {
     }
   });
 
+  /* chiamata get di un singolo autore */
   authorRoute.get("/:id", async (req, res, next) => {
     try {
       let author = await Author.findById(req.params.id);
@@ -31,3 +33,20 @@ authorRoute.get("/", async (req, res, next) => {
       next(err);
     }
   });
+
+/* richiesta di delete di un singolo autore */
+  authorRoute.delete("/:id", async (req, res, next) => {
+    try {
+      await Author.deleteOne({
+        _id: req.params.id,
+      });
+      res.send("L'utente è stato eliminato correttamente").status(204);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  /* richiesta PUT di un singolo autore */
+
+  
+
